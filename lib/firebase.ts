8537a384c7345533cb433bb4677c3dd53ba4212d
sdsk681-code <sdsk681@gmail.com>
2026-07-14 20:32:07 +0300

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -5,14 +5,14 @@ import { getDatabase } from "firebase/database";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyA-1PbZ9_iosGT5l7bXWiNNCmkaEikwNrI",
+  authDomain: "tahmeen-beker.firebaseapp.com",
+  databaseURL: "https://tahmeen-beker-default-rtdb.firebaseio.com",
+  projectId: "tahmeen-beker",
+  storageBucket: "tahmeen-beker.firebasestorage.app",
+  messagingSenderId: "875547234385",
+  appId: "1:875547234385:web:14ee88261bb3f3a2cd6870",
+  measurementId: "G-RDKYD2W845"
 };
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
