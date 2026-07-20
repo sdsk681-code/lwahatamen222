7b129eced96f1a592f5c2844b40ebab32c543d8f
sdsk681-code <sdsk681@gmail.com>
2026-07-20 09:37:29 +0300

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -5,14 +5,13 @@ import { getDatabase } from "firebase/database";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   // Paste config here
-  apiKey: "AIzaSyAVpibsrOcFd04Lks2wlVy7n3QFAqYsEas",
-  authDomain: "newap-main.firebaseapp.com",
-  databaseURL: "https://newap-main-default-rtdb.firebaseio.com",
-  projectId: "newap-main",
-  storageBucket: "newap-main.firebasestorage.app",
-  messagingSenderId: "677147669203",
-  appId: "1:677147669203:web:b0eda5cbda40f2b6521a26",
-  measurementId: "G-BSHEM91541"
+  authDomain: "tahmeen-s.firebaseapp.com",
+  databaseURL: "https://tahmeen-s-default-rtdb.firebaseio.com",
+  projectId: "tahmeen-s",
+  storageBucket: "tahmeen-s.firebasestorage.app",
+  messagingSenderId: "177307488051",
+  appId: "1:177307488051:web:9dc5be176e9be630d3b846",
+  measurementId: "G-7LQ2P5MC4T"
 };
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
